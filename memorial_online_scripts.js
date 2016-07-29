26cbda2db0fda85ee4f63606a84d2369ae13e00f
Mokrane <mmoali@quebec.cancer.ca>
2016-07-29 14:06:29 -0400

    - Add JS script to retreive URL Get parameters

--- memorial_online_scripts.js
+++ memorial_online_scripts.js
@@ -1,4 +1,4 @@
-// Canadian Cancer Society
+﻿// Canadian Cancer Society
 
 window.addEventListener('message', function(event) {
     if (~event.origin.indexOf('http://localhost:57072')) {
@@ -9,3 +9,22 @@ window.addEventListener('message', function(event) {
         return;
     }
 });
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}​
+
+console.log("------> Lanaguage = "+GetURLParameter("s_locale"));
+console.log("------> Company = "+GetURLParameter("s_orgGift"));
+console.log("------> Photo = "+GetURLParameter("moPersonImage"));
+console.log("------> Full Name = "+GetURLParameter("moPersonFullname"));
