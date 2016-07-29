378166824f2ad23cd92771a502b96481f5c7f0be
Mokrane <mmoali@quebec.cancer.ca>
2016-07-29 11:46:57 -0400

    - Add Quebec Style to donation form.

--- memorial_online_scripts.js
+++ memorial_online_scripts.js
@@ -1,2 +1,11 @@
 // Canadian Cancer Society
 
+window.addEventListener('message', function(event) {
+    if (~event.origin.indexOf('http://localhost:57072')) {
+        var frame = event.data;
+        frame.height = document.documentElement.scrollHeight;
+        console.log("-------->Scroll Height"+document.documentElement.scrollHeight;);
+    } else {
+        return;
+    }
+});
