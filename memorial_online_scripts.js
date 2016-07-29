aba0ae5e3d8dc32e731d00507f17996e1af5ad0f
Mokrane <mmoali@quebec.cancer.ca>
2016-07-29 14:20:34 -0400

    - Format input for money values

--- memorial_online_scripts.js
+++ memorial_online_scripts.js
@@ -10,6 +10,16 @@ window.addEventListener('message', function(event) {
     }
 });
 
+$('#level_standardexpanded41360amount').keypress(function (event) {
+    if (event.which <= 46 || event.which > 59) {
+        event.preventDefault();
+    }
+});
+$('#level_standardexpanded41360amount').bind('blur', function (e) {
+    if ($(this).val().length > 0) $(this).val(parseFloat($(this).val()).toFixed(2));
+
+});
+
 function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
