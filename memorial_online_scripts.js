c9de6ee1dfa935d2ba9a26b3180da76e266d901a
Mokrane <mmoali@quebec.cancer.ca>
2016-07-29 11:53:11 -0400

    Correction JS Script

--- memorial_online_scripts.js
+++ memorial_online_scripts.js
@@ -4,7 +4,7 @@ window.addEventListener('message', function(event) {
     if (~event.origin.indexOf('http://localhost:57072')) {
         var frame = event.data;
         frame.height = document.documentElement.scrollHeight;
-        console.log("-------->Scroll Height"+document.documentElement.scrollHeight;);
+        console.log("-------->Scroll Height"+document.documentElement.scrollHeight);
     } else {
         return;
     }
