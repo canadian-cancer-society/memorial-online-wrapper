0b3884b2e60bc218a2bd6ff8f0ca1cae475e6c38
Mokrane <mmoali@quebec.cancer.ca>
2017-03-03 15:55:09 -0500

    --

--- memorial_online_scripts.js
+++ memorial_online_scripts.js
@@ -2,17 +2,14 @@
 
 
 // Initialize tha page
-window.onload = initiate;
+//window.onload = initiate;
 
-//window.addEventListener('message', function(event) {
-//    if (~event.origin.indexOf('http://localhost:57072')) {
-//        var frame = event.data;
-//        frame.height = document.documentElement.scrollHeight;
-//        console.log("-------->Scroll Height"+document.documentElement.scrollHeight);
-//    } else {
-//        return;
-//    }
-//});
+$(document).ready(function () {
+
+
+    initiate();
+});
+
 
 $('#level_standardexpanded41360amount').keypress(function (event) {
     if (event.which <= 46 || event.which > 59) {
